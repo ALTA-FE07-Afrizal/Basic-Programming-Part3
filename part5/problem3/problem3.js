913e970502e9099061988b5f69b11b9379b0c3ee
unknown <4friizal@gmail.com>
2022-07-03 15:08:12 +0700

    file update

---
+++ part5/problem3/problem3.js
@@ -0,0 +1,21 @@
+function arrayUnique(arrayA, arrayB) {
+  // Your Code Here
+  let result = [];
+  for (let i = 0; i < arrayA.length; i++) {
+    let double = 0;
+    for (let j = 0; j < arrayA.length; j++) {
+      if (arrayA[i] == arrayB[j]) {
+        double += 1;
+      }
+    }
+    if (double == 0) {
+      result.push(arrayA[i]);
+    }
+    double = 0;
+  }
+  return result;
+}
+console.log(arrayUnique([1, 2, 3, 4], [1, 3, 5, 10, 16])); // [2 4]
+console.log(arrayUnique([10, 20, 30, 40], [5, 10, 15, 59])); // [20 30 40]
+console.log(arrayUnique([1, 3, 7], [1, 3, 5])); // [7]
+console.log(arrayUnique([3, 8], [2, 8])); // [3]
